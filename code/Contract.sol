58df051e1a71f2db8437de0ceafc702192563ef3
lmoheyma <louismoheymani@gmail.com>
2024-12-29 17:27:40 +0100

    contract name + version

--- code/Contract.sol
+++ code/Contract.sol
@@ -1,21 +1,21 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract GameItem is ERC721URIStorage {
+contract GBS42 is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     constructor() ERC721("GBS42", "GBS") {}
 
-    function awardItem(address player, string memory tokenURI)
+    function awardItem(address to, string memory tokenURI)
         public
         returns (uint256)
     {
         uint256 newItemId = _tokenIds.current();
-        _mint(player, newItemId);
+        _mint(to, newItemId);
         _setTokenURI(newItemId, tokenURI);
 
         _tokenIds.increment();
